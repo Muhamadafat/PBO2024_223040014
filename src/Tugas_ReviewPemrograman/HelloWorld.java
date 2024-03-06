170c6759ed1ee425e7354456c89af0dd8ba60966
muhamad afat <>
2024-03-06 17:44:33 +0700

    upload tugas

---
+++ src/Tugas_ReviewPemrograman/HelloWorld.java
@@ -0,0 +1,9 @@
+package Tugas_ReviewPemrograman;
+// Ini merupakan contoh pemrograman yang menampilkan kalimat 
+// Hello World! dengan menggunakan komentar program
+
+public class HelloWorld {
+	public static void main (String[]args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ src/Tugas_ReviewPemrograman/Penjumlahan.java
@@ -0,0 +1,13 @@
+package Tugas_ReviewPemrograman;
+//Ini merupakan contoh pemrograman yang menampilkan Penjumlahan
+
+public class Penjumlahan {
+	public static  void main (String[]args) {
+		int a=4;
+		int b=3;
+		int c= a+b;
+		
+		
+		System.out.println("Hasil dari Penjumlahan :" +c);
+	}
+}
